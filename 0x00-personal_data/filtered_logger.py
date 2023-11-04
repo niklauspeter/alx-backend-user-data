d6aa3ad2ec0bb5e3d79552b5d68f70ddada13815
klaus <oriokiklaus@gmail.com>
2023-11-04 14:23:35 +0300

    implement a get db function which returns connector to database os module obtains credentials from the environment and mysql-connector-python module to connect to mysql database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,6 +53,22 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Implement db conectivity
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
+
+
+
 
 
 if __name__ == '__main__':
