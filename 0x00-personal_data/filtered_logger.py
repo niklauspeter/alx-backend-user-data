81ed41b81a4c5b7c956e5d3292c93ae858b69795
klaus <oriokiklaus@gmail.com>
2023-11-04 14:21:36 +0300

    implement get logger function that doesn;t propagate messages to other loggers

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,6 +40,19 @@ class RedactingFormatter(logging.Formatter):
                             super().format(record), self.SEPARATOR)
 
 
+def get_logger() -> logging.Logger:
+    """ Implementing a logger.
+    """
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
 
 
 if __name__ == '__main__':
