7807ad4effd767d195dba008e2613779d5b421bc
klaus <oriokiklaus@gmail.com>
2023-11-04 13:21:51 +0300

    add function filter_datum that returns log message obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Personal Data
+"""
+
+import logging
+import os
+import re
+from typing import List
+import mysql.connector
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+if __name__ == '__main__':
+    main()
