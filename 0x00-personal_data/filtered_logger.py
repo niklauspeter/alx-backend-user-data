db21a58680b5e5eca85ead87ce955833836f1330
klaus <oriokiklaus@gmail.com>
2023-11-04 14:19:51 +0300

    add redacting formatter class that accept list of strings implement format method to filter values inincomeing log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,5 +22,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
+class RedactingFormatter(logging.Formatter):
+    """ RedactingFormatter class. """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+
+
 if __name__ == '__main__':
     main()
